a12aa7531e2e1ead359f23b15eb9ce8e3a46a43c
aldinalj <aldinalj@gmail.com>
2024-12-11 16:17:18 +0100

    Inital commit

---
+++ src/test/java/com/aldinalj/triptip/TripTipApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aldinalj.triptip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TripTipApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
