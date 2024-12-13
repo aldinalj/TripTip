840649aabe2dc28bd44f4509bf6abb90ca9fa870
aldinalj <aldinalj@gmail.com>
2024-12-13 16:24:56 +0100

    Add account status fields and user role to CustomUser entity

--- src/main/java/com/aldinalj/triptip/user/model/CustomUser.java
+++ src/main/java/com/aldinalj/triptip/user/model/CustomUser.java
@@ -1,5 +1,6 @@
 package com.aldinalj.triptip.user.model;
 
+import com.aldinalj.triptip.user.authorities.UserRole;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
@@ -28,12 +29,25 @@ public class CustomUser {
     @Size(min = 8, max = 30, message = "Length must be between 8-30 characters long")
     private String password;
 
+    @Enumerated(EnumType.STRING)
+    private UserRole userRole;
+    private boolean isAccountNonExpired;
+    private boolean isAccountNonLocked;
+    private boolean isCredentialsNonExpired;
+    private boolean isEnabled;
+
+
     public CustomUser() {}
 
-    public CustomUser(String displayName, String email, String password) {
+    public CustomUser(String displayName, String email, String password, UserRole userRole, boolean isAccountNonExpired, boolean isAccountNonLocked, boolean isCredentialsNonExpired, boolean isEnabled) {
         this.displayName = displayName;
         this.email = email;
         this.password = password;
+        this.userRole = userRole;
+        this.isAccountNonExpired = isAccountNonExpired;
+        this.isAccountNonLocked = isAccountNonLocked;
+        this.isCredentialsNonExpired = isCredentialsNonExpired;
+        this.isEnabled = isEnabled;
     }
 
     public Long getId() {
@@ -62,5 +76,45 @@ public class CustomUser {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public UserRole getUserRole() {
+        return userRole;
+    }
+
+    public void setUserRole(UserRole userRole) {
+        this.userRole = userRole;
+    }
+
+    public boolean isAccountNonExpired() {
+        return isAccountNonExpired;
+    }
+
+    public void setAccountNonExpired(boolean accountNonExpired) {
+        isAccountNonExpired = accountNonExpired;
+    }
+
+    public boolean isAccountNonLocked() {
+        return isAccountNonLocked;
+    }
+
+    public void setAccountNonLocked(boolean accountNonLocked) {
+        isAccountNonLocked = accountNonLocked;
+    }
+
+    public boolean isCredentialsNonExpired() {
+        return isCredentialsNonExpired;
+    }
+
+    public void setCredentialsNonExpired(boolean credentialsNonExpired) {
+        isCredentialsNonExpired = credentialsNonExpired;
+    }
+
+    public boolean isEnabled() {
+        return isEnabled;
+    }
+
+    public void setEnabled(boolean enabled) {
+        isEnabled = enabled;
     }
 }
